15b72a3ba288fa8f8c7969d2c5a9ed26874d88ed
maany <imptodefeat@gmail.com>
2018-04-30 16:29:08 +0200

    contd..

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,8 @@ node default {
 }
 
 node 'ip-172-31-36-145.eu-central-1.compute.internal'{
-	include role::master_server
+	#include role::master_server
+	include role::central_configuration_server
 }
 node /^web/ {
 	include role::app_server
